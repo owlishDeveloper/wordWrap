9fda1f520d9e55dc55c37f7ddc21942557aa7649
Ammar Vahanvaty <ammar@stage3systems.com>
2017-04-24 17:11:11 -0700

    Naive implementation of wordWrap

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-'use strict';
+'use strict'
+const fs = require('fs');
+const path = require('path');
+const wordWrap = require('./lib/word_wrap').wordWrap;
 
-console.log('Hello World!');
+/**
+ * This function is expected to take a string and a integer, and output a new string which wraps the string
+ * at the given number of characters
+ * For Example, given a value of 5, every 5th character must wrap and start a new line
+ */
+
+//Insert string from file and value from command line
+const stringFile = process.argv[2];
+const value = process.argv[3];
+
+const stringContents = fs.readFileSync(`${__dirname}/${stringFile}`, 'utf8');
+
+wordWrap(stringContents, value);
+
+
+
